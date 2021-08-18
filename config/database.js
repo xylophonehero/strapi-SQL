b8b46dd8878eb3897cd4fe8c1ae90963f9b814d5
XylophoneHero <nick97000@gmail.com>
2021-08-18 15:25:38 +0100

    removed int from database.js

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'mysql',
         host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
+        port: env('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
