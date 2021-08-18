300f83f53f996862c72c715cf16de7727f3ad4af
XylophoneHero <nick97000@gmail.com>
2021-08-18 14:12:52 +0100

    removed exposed vars

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'strapi-dev1.ctkkdi0tsumb.eu-west-1.rds.amazonaws.com'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi-dev'),
-        username: env('DATABASE_USERNAME', 'admin'),
-        password: env('DATABASE_PASSWORD', 'JOv8QBl8Jxb11n9DPM4U'),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         ssl: false
